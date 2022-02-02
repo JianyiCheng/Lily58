34dad03ce3753bc1032a9fe1fe51050624a5d615
Jianyi Cheng <jc9016@ic.ac.uk>
2022-02-02 12:06:09 +0000

    Corrected keymaps

--- mydesigns/mykeymap/keymap.c
+++ mydesigns/mykeymap/keymap.c
@@ -1,17 +1,68 @@
-// Jianyi's keymap
+/* Attribution-ShareAlike 4.0 International (CC BY-SA 4.0) 2020 Max Drasbeck
+ *
+ * You are free to:
+ *
+ *  Share — copy and redistribute the material in any medium or format
+ *  Adapt — remix, transform, and build upon the material
+ *   for any purpose, even commercially.
+ *
+ * The licensor cannot revoke these freedoms as long as you follow the license terms.
+ */
 
 #include QMK_KEYBOARD_H
 
-enum layer_number {
-  _QWERTY = 0,
+extern uint8_t is_master;
+
+enum layers {
+  _QWERTY,
   _LOWER,
   _RAISE,
   _ADJUST,
 };
 
+enum custom_keycodes {
+  QWERTY = SAFE_RANGE,
+  LOWER,
+  RAISE,
+  ADJUST,
+};
+
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-/* QWERTY
+  // [_QWERTY] = LAYOUT(
+  // KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+  // KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
+  // KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  // KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_NO,   KC_MUTE, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT,
+  //                            KC_LALT, KC_LGUI, MO(1),   KC_SPC,  KC_SPC,  MO(2),   KC_NUBS, KC_LCTL
+  // ),
+
+  // [_LOWER] = LAYOUT(
+  // KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_DEL,
+  // KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                     KC_NO,   KC_NO,   KC_PGUP, KC_MINS, KC_EQL,  KC_RBRC,
+  // KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_NO,   KC_HOME, KC_PGDN, KC_END,  KC_NO,   KC_NUHS,
+  // KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_RO,   KC_ENT,
+  //                            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+  // ),
+
+  // [_RAISE] = LAYOUT(
+  // KC_TRNS, RALT(KC_1), RALT(KC_2), RALT(KC_3), RALT(KC_4), RALT(KC_5),                RALT(KC_6), RALT(KC_7),    RALT(KC_8), RALT(KC_9), RALT(KC_0), KC_DEL,
+  // KC_TRNS, KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,                     KC_NO,      KC_NO,         KC_UP,      KC_MINS,    KC_EQL,     KC_RBRC,
+  // KC_ESC,  KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,                     KC_NO,      KC_LEFT,       KC_DOWN,    KC_RGHT,    KC_NO,      KC_NUHS,
+  // KC_LSFT, KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,   KC_NO,   KC_NO,   KC_NO,      KC_VOLD,       KC_VOLU,    KC_MUTE,    KC_RO,      KC_SFTENT,
+  //                                  KC_RALT,    KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    RALT(KC_NUBS), KC_TRNS
+  // ),
+
+  // [_ADJUST] = LAYOUT(
+  // XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  // XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  // XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI,
+  // XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD,
+  //                            _______, _______, _______, _______,  _______,  _______, _______, _______
+  // )
+
+ /* QWERTY
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  `   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
@@ -25,8 +76,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
- // left fn could be another layer - todo
- [_QWERTY] = LAYOUT(
+ // left fn could be the raise layer - todo
+  [_QWERTY] = LAYOUT(
   KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
   KC_LCTRL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
@@ -51,7 +102,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
   KC_LCAP, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_BTN1,                   _______, KC_PSCR, KC_PSLS, KC_PAUS, KC_UP,  KC_DEL,
   _______, KC_KB_VOLUME_UP,  KC_KB_VOLUME_DOWN, KC_KB_MUTE, KC_EJCT, _______,      KC_PAST, KC_AMPR, KC_HOME, KC_PGUP, KC_LEFT, KC_RGHT,
-  _______, S(C(KC_TAB)), C(KC_TAB), A(KC_TAB), G(KC_TAB), _______,        KC_RBRC, KC_PPLS, KC_PMNS, KC_END,  KC_PGDN, KC_DOWN, _______,
+  _______, S(C(KC_TAB)), C(KC_TAB), A(KC_TAB), G(KC_TAB), _______,  _______,       KC_RBRC, KC_PPLS, KC_PMNS, KC_END,  KC_PGDN, KC_DOWN, _______,
                             _______, _______, _______, _______, _______,  _______, KC_INS,  KC_STOP
 ),
 /* RAISE
@@ -99,8 +150,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   )
 };
 
-layer_state_t layer_state_set_user(layer_state_t state) {
-  return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
+int RGB_current_mode;
+
+void matrix_init_user(void) {
+#ifdef RGBLIGHT_ENABLE
+    RGB_current_mode = rgblight_config.mode;
+#endif
 }
 
 //SSD1306 OLED update loop, make sure to enable OLED_ENABLE=yes in rules.mk
@@ -116,8 +171,8 @@ oled_rotation_t oled_init_user(oled_rotation_t rotation) {
 const char *read_layer_state(void);
 const char *read_logo(void);
 void set_keylog(uint16_t keycode, keyrecord_t *record);
-const char *read_keylog(void);
-const char *read_keylogs(void);
+//const char *read_keylog(void);
+//const char *read_keylogs(void);
 
 // const char *read_mode_icon(bool swap);
 // const char *read_host_led_state(void);
@@ -128,8 +183,8 @@ bool oled_task_user(void) {
   if (is_keyboard_master()) {
     // If you want to change the display of OLED, you need to change here
     oled_write_ln(read_layer_state(), false);
-    oled_write_ln(read_keylog(), false);
-    oled_write_ln(read_keylogs(), false);
+    //oled_write_ln(read_keylog(), false);
+    //oled_write_ln(read_keylogs(), false);
     //oled_write_ln(read_mode_icon(keymap_config.swap_lalt_lgui), false);
     //oled_write_ln(read_host_led_state(), false);
     //oled_write_ln(read_timelog(), false);
@@ -147,5 +202,76 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 #endif
     // set_timelog();
   }
+
+  // switch (keycode) {
+  //   case QWERTY:
+  //     if (record->event.pressed) {
+  //       set_single_persistent_default_layer(_QWERTY);
+  //     }
+  //     return false;
+  //     break;
+  //   case LOWER:
+  //     if (record->event.pressed) {
+  //       layer_on(_LOWER);
+  //     } else {
+  //       layer_off(_LOWER);
+  //     }
+  //     update_tri_layer(_LOWER, _RAISE, _ADJUST);
+  //     return false;
+  //     break;
+  //   case RAISE:
+  //     if (record->event.pressed) {
+  //       layer_on(_RAISE);
+  //     } else {
+  //       layer_off(_RAISE);
+  //     }
+  //     update_tri_layer(_LOWER, _RAISE, _ADJUST);
+  //     return false;
+  //     break;
+  //   case ADJUST:
+  //       if (record->event.pressed) {
+  //         layer_on(_ADJUST);
+  //       } else {
+  //         layer_off(_ADJUST);
+  //       }
+  //       return false;
+  //       break;
+  // }
   return true;
 }
+
+#ifdef ENCODER_ENABLE
+bool encoder_update_user(uint8_t index, bool clockwise) {
+  // // index 1 == minion side
+  // if (index == 1) {
+  //   if (clockwise) {
+  //     tap_code(KC_VOLD);
+  //   } else {
+  //     tap_code(KC_VOLU);
+  //   }
+  // }
+  uint8_t temp_mod = get_mods();
+    uint8_t temp_osm = get_oneshot_mods();
+    // bool    is_ctrl  = (temp_mod | temp_osm) & MOD_MASK_CTRL;
+    bool    is_shift = (temp_mod | temp_osm) & MOD_MASK_SHIFT;
+    if (is_shift) {
+        if (index == 0) {
+            if (clockwise) {
+                tap_code(KC_WH_R);
+            } else {
+                tap_code(KC_WH_L);
+            }
+        }
+    }
+    else {
+        if (index == 0) {
+            if (clockwise) {
+                tap_code(KC_WH_D);
+            } else {
+                tap_code(KC_WH_U);
+            }
+        }
+    }
+    return true;
+}
+#endif
--- mydesigns/mykeymap/rules.mk
+++ mydesigns/mykeymap/rules.mk
@@ -1,14 +1,23 @@
+# Build Options
+#   change yes to no to disable
+#
 BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = yes        # Mouse keys
-EXTRAKEY_ENABLE = no        # Audio control and System control
+EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
-NKRO_ENABLE = no
+NKRO_ENABLE = no            # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 AUDIO_ENABLE = no           # Audio output
-RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight.
+BLUETOOTH_ENABLE = no       # Enable Bluetooth
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 SWAP_HANDS_ENABLE = no      # Enable one-hand typing
-OLED_ENABLE= no     # OLED display
+OLED_ENABLE = no 
+OLED_DRIVER = SSD1306    # OLED display
+ENCODER_ENABLE = yes        # Enable encoder
+
+# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
+SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
 
 # If you want to change the display of OLED, you need to change here
 SRC +=  ./lib/rgb_state_reader.c \
